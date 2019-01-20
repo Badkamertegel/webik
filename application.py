9cd7648bef1e5941b658308d450f9cbbc697fec7
Kevin Tran <kevintran.1597@gmail.com>
2019-01-20 15:19:48 +0000

    static

--- application.py
+++ application.py
@@ -37,6 +37,7 @@ print(categorie, vraag, goed_antwoord, foute_antwoorden, moeilijkheidsgraad)
 
 
 @app.route("/create")
+@login_required
 def create():
 
     if request.method =="POST":
@@ -48,6 +49,7 @@ def create():
         return render_template("create.html", category=categorie, vraag=vraag, goed=goed_antwoord, fout=foute_antwoorden, moeilijkheidsgraad=moeilijkheidsgraad, categories=categories, difficulty=difficulty)
 
 @app.route("/game")
+@login_required
 def game():
 
     return render_template("game.html")
@@ -71,20 +73,16 @@ def register():
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("both passwords must be the same")
 
-        password = request.form.get("password")
-        hash = pwd_context.hash(password)
 
-        result = db.execute("INSERT INTO user2 (username, hash) VALUES (:username, :password)",
-                            username=request.form.get("username"), password=hash)
+        result = db.execute("INSERT INTO user (username, hash) VALUES (:username, :hash)",
+                            username=request.form.get("username"), hash=pwd_context.hash(request.form.get("password")))
 
         if not result:
             return apology("username is already in use")
-        else:
-            return apology("registration complete", 200)
 
         session["user_id"] = result
 
-        return redirect(url_for("index"))
+        return redirect(url_for("homepage"))
     else:
         return render_template("register.html")
 
@@ -100,7 +98,7 @@ def login():
     # if user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # ensure username was submitted
+     #ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username")
 
@@ -108,18 +106,18 @@ def login():
         elif not request.form.get("password"):
             return apology("must provide password")
 
-        # query database for username
-        rows = db.execute("SELECT * FROM user2 WHERE username = :username", username=request.form.get("username"))
+         # query database for username
+        rows = db.execute("SELECT * FROM user WHERE username = :username", username=request.form.get("username"))
 
-        # ensure username exists and password is correct
+         # ensure username exists and password is correct
         if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-            return "invalid username and/or password"
+             return "invalid username and/or password"
 
-        # remember which user has logged in
+         # remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
         # redirect user to home page
-        return redirect(url_for("login"))
+        return redirect(url_for("homepage"))
 
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
@@ -192,17 +190,20 @@ def teacherRegister():
     else:
         return render_template("register.html")
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/homepage", methods=["GET","POST"])
+@login_required
 def homepage():
 
-    return render_template("login.html")
+    return render_template("homepage.html")
 
 @app.route("/result_student", methods=["GET","POST"])
+@login_required
 def result_student():
 
     return render_template("result_student.html")
 
 @app.route("/result_teacher", methods=["GET","POST"])
+@login_required
 def result_teacher():
 
     return render_template("result_teacher.html")
