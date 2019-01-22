6949ae1c3abba839979cd3507a394aefe05982d6
Kevin Tran <kevintran.1597@gmail.com>
2019-01-22 13:26:04 +0000

    templates

--- application.py
+++ application.py
@@ -18,22 +18,29 @@ app.secret_key = 'my unobvious secret key'
 db = SQL("sqlite:///project.db")
 
 data = Trivia(True)
-response = data.request(2, Category.History ,Diffculty.Hard, Type.Multiple_Choice)
+response = data.request(1, Category.History ,Diffculty.Hard, Type.Multiple_Choice)
 informatie = []
-categories = ["Maths", "History"]
-difficulty = ["Easy", "Medium", "Hard"]
 
 for info in response['results']:
     informatie.append(info)
 
-for element in range(len(informatie)):
-    categorie = informatie[element]['category']
-    vraag = informatie[element]['question']
-    goed_antwoord = informatie[element]['correct_answer']
-    foute_antwoorden = informatie[element]['incorrect_answers']
-    moeilijkheidsgraad = informatie[element]['difficulty']
+categorie = []
+vraag = []
+goed_antwoord = []
+foute_antwoorden = []
+moeilijkheidsgraad = []
 
-print(categorie, vraag, goed_antwoord, foute_antwoorden, moeilijkheidsgraad)
+for element in range(len(informatie)):
+    categorie.append(informatie[element]['category'])
+    vraag.append(informatie[element]['question'])
+    goed_antwoord.append(informatie[element]['correct_answer'])
+    foute_antwoorden.append(informatie[element]['incorrect_answers'])
+    moeilijkheidsgraad.append(informatie[element]['difficulty'])
+
+print(foute_antwoorden[0][0])
+
+
+
 
 
 @app.route("/create")
@@ -42,17 +49,33 @@ def create():
 
     if request.method =="POST":
 
+        teacher_value = db.execute("SELECT teacher FROM user WHERE username = :username", username=request.form.get("username"))
+
+        if teacher_value == 1:
+            session["user_id"] = user
+        else:
+            session["user_id"] = teacher
+
         return redirect(url_for("create.html"))
+
 
     else:
         # print(categorie)
-        return render_template("create.html", category=categorie, vraag=vraag, goed=goed_antwoord, fout=foute_antwoorden, moeilijkheidsgraad=moeilijkheidsgraad, categories=categories, difficulty=difficulty)
+        return render_template("create.html", category=categorie, vraag=vraag, goed=goed_antwoord, fout=foute_antwoorden, moeilijkheidsgraad=moeilijkheidsgraad)
 
 @app.route("/game")
 @login_required
 def game():
 
-    return render_template("game.html")
+    if request.method == "GET":
+
+        counter = 0
+
+        if request.form.get("goed"):
+            counter += 1
+
+
+        return render_template("game.html", categorie=categorie[0], vraag = vraag[0], moeilijkheidsgraad = moeilijkheidsgraad[0], goed = goed_antwoord[0], fout_1 = foute_antwoorden[0][0], fout_2 = foute_antwoorden[0][1], fout_3 = foute_antwoorden[0][2])
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -73,9 +96,14 @@ def register():
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("both passwords must be the same")
 
+        if request.form.get("checkbox"):
+            teacher = 1
+        else:
+            teacher = 0
 
-        result = db.execute("INSERT INTO user (username, hash) VALUES (:username, :hash)",
-                            username=request.form.get("username"), hash=pwd_context.hash(request.form.get("password")))
+
+        result = db.execute("INSERT INTO user (username, hash, teacher) VALUES (:username, :hash, :teacher)",
+                            username=request.form.get("username"), hash=pwd_context.hash(request.form.get("password")), teacher = teacher)
 
         if not result:
             return apology("username is already in use")
@@ -111,10 +139,12 @@ def login():
 
          # ensure username exists and password is correct
         if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-             return "invalid username and/or password"
+             return "invalid username and/or password and/or teacher"
+
 
          # remember which user has logged in
         session["user_id"] = rows[0]["id"]
+
 
         # redirect user to home page
         return redirect(url_for("homepage"))
@@ -122,73 +152,6 @@ def login():
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
-
-@app.route("/teacherlogin", methods=["GET","POST"])
-def teacherlogin():
-
-    session.clear()
-
-
-    if request.method == "POST":
-
-
-        if not request.form.get("username"):
-            return apology("must provide username")
-
-
-        elif not request.form.get("password"):
-            return apology("must provide password")
-
-
-        rows = db.execute("SELECT * FROM teachers WHERE username = :username", username=request.form.get("username"))
-
-
-        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-            return "invalid username and/or password"
-
-
-        session["user_id"] = rows[0]["id"]
-
-
-        return redirect(url_for("index"))
-
-
-    else:
-        return render_template("login.html")
-
-@app.route("/teacherRegister", methods=["GET","POST"])
-def teacherRegister():
-    if request.method == "POST":
-
-        if not request.form.get("username"):
-            return apology("must choose a username")
-
-        elif not request.form.get("password"):
-            return apology("must choose a password")
-
-        elif not request.form.get("confirmation"):
-            return apology("must fill in password again")
-
-        elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("both passwords must be the same")
-
-        password = request.form.get("password")
-        hash = pwd_context.hash(password)
-
-        result = db.execute("INSERT INTO teachers (username, hash) VALUES (:username, :password)",
-                            username=request.form.get("username"), password=hash)
-
-        if not result:
-            return apology("username is already in use")
-        else:
-            return apology("registration complete", 200)
-
-        session["user_id"] = result
-
-        return redirect(url_for("index"))
-    else:
-        return render_template("register.html")
 
 @app.route("/homepage", methods=["GET","POST"])
 @login_required
@@ -200,10 +163,14 @@ def homepage():
 @login_required
 def result_student():
 
-    return render_template("result_student.html")
+    return render_template("result_student.html", categorie = categorie, moeilijkheidsgraad = moeilijkheidsgraad, counter = counter)
 
 @app.route("/result_teacher", methods=["GET","POST"])
 @login_required
 def result_teacher():
+    if request.method == "POST":
 
-    return render_template("result_teacher.html")
+        return render_template("result_teacher.html")
+
+
+
